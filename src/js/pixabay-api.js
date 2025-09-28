8a5ad1b6903acc4aa0b7600d05563be6431767fd
Angi-rosmari <anzhelarebii@gmail.com>
2025-09-28 17:42:42 +0300

    add files

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+export default getImagesByQuery;
+/* let page = 10 */
+
+async function getImagesByQuery(query, page = 1) {
+  const resp = await axios({
+    method: 'get',
+    url: 'https://pixabay.com/api/',
+    params: {
+      key: '28573378-d6ea96aa58230c7b9cb77205d',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page,
+    },
+  });
+  return resp.data;
+}
